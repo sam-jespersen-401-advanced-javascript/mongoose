2119567c785d655ee35cb231c0f6ffa787b17075
Sam Jespersen <heyheysamj@gmail.com>
2019-09-25 15:55:34 -0700

    marty's magic code fix

--- e2e/setup.js
+++ e2e/setup.js
@@ -3,7 +3,7 @@ const TEST_DB_URL = 'mongodb://localhost:27017/languages';
 const mongoose = require('mongoose');
 
 beforeAll(() => {
-  connect(TEST_DB_URL, { log: false });
+  return connect(TEST_DB_URL, { log: false });
 });
 
 afterAll(() => {
